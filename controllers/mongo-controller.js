5e430e51750b3af6f922eb1a1b7a0f3921d584d5
Bob Brown <robertbernardbrown@gmail.com>
2018-04-06 12:23:38 -0400

    refactors mongoose scrape code

--- controllers/mongo-controller.js
+++ controllers/mongo-controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router  = express.Router();
-const getData = require("./scrape");
+const data = require("./scrape");
 
 ///////////////////
 //ROUTE FUNCTIONS//
---
+++ models/Article.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema ({
+    author: {type: String},
+    title: {type: String},
+    image: {type: String},
+    date: {type: String},
+    url: {type: String},
+  })
+
+var Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
