9419c6c35ea5d988e043a5497ee584eea32fd347
Bob Brown <robertbernardbrown@gmail.com>
2018-04-06 14:31:37 -0400

    adds validation to model to not allow new entries in DB without a unique URL

--- models/Article.js
+++ models/Article.js
@@ -2,11 +2,22 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var articleSchema = new Schema ({
-    author: {type: String},
-    title: {type: String},
-    image: {type: String},
-    date: {type: String},
-    url: {type: String},
+    author: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    date: {
+      type: String
+    },
+    url: {
+      type: String,
+      unique: true
+    },
   })
 
 var Article = mongoose.model("Article", articleSchema);
